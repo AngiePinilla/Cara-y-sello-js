c32ff24bb0a4b01f8cc168d4bb7da725c48f0aa4
AngiePinilla <104436269+AngiePinilla@users.noreply.github.com>
2022-11-01 20:22:30 -0500

    prototipo

--- code.js
+++ code.js
@@ -1,51 +1,81 @@
 
 let play = document.getElementById("btn");
+let reset = document.getElementById("reset");
+let gano = document.getElementById("cont-gano");
+let perdio = document.getElementById("cont-perdio");
+
+let resu = document.getElementById("resultados");
+
+
+
 let apuesta = 0;
 let cont = 0;
 let gana = 0;
 let pierde = 0;
 
 function Jugar() {
-    
+
     apuesta = document.getElementById("valor").value;
     let opcion = parseInt(document.getElementById("opc").value);
-    
+
     let seguir = true;
     do {
-        
+
 
         let alt = 1 + (parseInt(Math.random() * 2));
 
+        
+
         if (opcion == alt) {
-            
+
             apuesta = apuesta * 2;
             cont = cont + 1;
             gana = gana + 1;
             alert(`Gano apuesta: ${apuesta}, contador:${cont}`)
+            
+
 
         } else if (opcion != alt) {
-            
-            apuesta = apuesta / 2;
+
+            apuesta = apuesta - 1;
             cont = cont + 1;
             pierde = pierde + 1;
             alert(`Perdio apuesta: ${apuesta}, contador: ${cont}`)
-        } else {
-            alert(`no valido`)
-        }
+
+
+        } 
+
+        
 
         seguir = confirm(`Seguir`);
         console.log(`Estado ${seguir}`)
-        
-       
+
+
     } while (seguir != false);
 
-    alert(`Las partidas jugadas fueron ${cont} 
-                Dinero acumulado ${apuesta}
-                Partidas ganadas ${gana}
-                Partdias perdidas ${pierde}`)
+    resu.innerHTML = `<div>
+    <h4>Resultados</h4>
+    <p>Las partidas jugadas fueron ${cont}, Dinero acumulado ${apuesta} </p>
+
+    </div>`
+
+    gano.innerHTML = `<p>${gana}</p>`;
+    perdio.innerHTML = `<p>${pierde}</p>`;
+
 
     console.log(opcion)
     console.log(apuesta)
 }
 
 play.addEventListener('click', Jugar);
+
+function recargar() {
+
+    resu.innerHTML = ``
+
+    gano.innerHTML = ``;
+    perdio.innerHTML = ``;
+    
+}
+
+reset.addEventListener('click', recargar);
