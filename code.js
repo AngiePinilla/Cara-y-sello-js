f5317ca8d5dbf238b582ce47d6b02de406b130ab
AngiePinilla <104436269+AngiePinilla@users.noreply.github.com>
2022-11-02 23:47:56 -0500

    Juego finalizado

--- code.js
+++ code.js
@@ -1,80 +1,99 @@
+/*
+Nombre: code.js
+Version: 1.0
+Autor: Angie p.
+ */
 
 let play = document.getElementById("btn");
 let reset = document.getElementById("reset");
 let gano = document.getElementById("cont-gano");
 let perdio = document.getElementById("cont-perdio");
-
+let replay = document.getElementById("volver");
 let resu = document.getElementById("resultados");
+let prim = document.getElementById("priresu");
+
+console.log(replay)
 
 
-
-let apuesta = 0;
 let cont = 0;
 let gana = 0;
 let pierde = 0;
+let suma = 0;
 
+/*Funcion Jugar para ejecutar el juego */
 function Jugar() {
 
-    apuesta = document.getElementById("valor").value;
+    let apuesta = document.getElementById("valor").value;
     let opcion = parseInt(document.getElementById("opc").value);
+    // let opc = parseInt(document.getElementById("decision").value);
 
     let seguir = true;
-    do {
 
+    if (apuesta <= 0) {
+        Swal.fire(`No valido ingrese un valor para apostar`)
+    }
+
+    apuesta = parseInt(apuesta)
+
+    if (opcion <= 0) {
+        Swal.fire(`No valido, elija un opcion`)
+    }
+    
+    while (seguir != false && apuesta >= 1 && opcion >= 1) {
 
         let alt = 1 + (parseInt(Math.random() * 2));
-
-        
+        console.log(`alt ${alt}`)
 
         if (opcion == alt) {
 
-            apuesta = apuesta * 2;
+            suma = suma + apuesta;
             cont = cont + 1;
             gana = gana + 1;
-            alert(`Gano apuesta: ${apuesta}, contador:${cont}`)
-            
+            // alert(`Gano apuesta: ${apuesta}, contador:${cont}`)
 
+            prim.innerHTML = `<div><h6>¡Felicitaciones! Ganaste</h6>
+            <p>El dinero obtenido es : ${suma}</p></div>`;
 
         } else if (opcion != alt) {
-
-            apuesta = apuesta - 1;
+            suma = suma - apuesta;
             cont = cont + 1;
             pierde = pierde + 1;
-            alert(`Perdio apuesta: ${apuesta}, contador: ${cont}`)
-
-
+            prim.innerHTML = `<div><h6>Lo sentimos, Perdiste :c</h6>
+            <p>El dinero obtenido es : ${suma}</p></div>`;
         } 
-
         
 
-        seguir = confirm(`Seguir`);
-        console.log(`Estado ${seguir}`)
+        seguir = false;
 
+    } 
 
-    } while (seguir != false);
-
-    resu.innerHTML = `<div>
-    <h4>Resultados</h4>
-    <p>Las partidas jugadas fueron ${cont}, Dinero acumulado ${apuesta} </p>
-
+    resu.innerHTML = `<div class="resul">
+    <h2 class="cont-titulo">Resultados</h2>
+    <p>Las partidas jugadas fueron ${cont}, <br> 
+    Dinero Inicial ${apuesta}<br> 
+    Dinero acumulado ${suma} </p>
     </div>`
 
     gano.innerHTML = `<p>${gana}</p>`;
     perdio.innerHTML = `<p>${pierde}</p>`;
 
 
-    console.log(opcion)
-    console.log(apuesta)
+    console.log(`opcion ${opcion}`)
+    console.log(`apuesta ${apuesta}`)
 }
 
 play.addEventListener('click', Jugar);
 
 function recargar() {
 
-    resu.innerHTML = ``
-
+    resu.innerHTML = ``;
     gano.innerHTML = ``;
     perdio.innerHTML = ``;
+    prim.innerHTML = ``;
+    cont = 0;
+    suma = 0;
+    gana = 0;
+    pierde = 0;
     
 }
 
